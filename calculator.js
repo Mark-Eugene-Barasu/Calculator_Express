02e23cbfbbecb7cdf8964a74021d1c611d968700
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-22 14:20:17 +0200

    install body-parser to use input values from form to use them in server side computation

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -7,16 +8,20 @@ app.get("/", function(req, res) {
     res.sendFile(`${__dirname}/index.html`);
 });
 
+app.post("/", function(req, res){
+    res.send(`Thank you`);
+})
+
+
 app.listen(3000, function(){
     console.log(`server running on port: 3000`);
 });
 
 
-```
-to actually print out a file path
-=> console.log(__dirname)
 
-in terminal it is
-=> pwd
+//to actually print out a file path
+// => console.log(__dirname)
 
-```
+// in terminal it is
+// => pwd
+
