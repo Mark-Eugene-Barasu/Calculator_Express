f9ec735fc1f1e9367777c79a5bc19bab15e8833d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-22 13:59:57 +0200

    the use of __dirname to retrieve file path

--- calculator.js
+++ calculator.js
@@ -4,9 +4,19 @@ const app = express();
 
 app.get("/", function(req, res) {
     // a Respond to display on the browser
-    res.send(`Hello World`);
-})
+    res.sendFile(`${__dirname}/index.html`);
+});
 
 app.listen(3000, function(){
-    console.log(`server running`);
+    console.log(`server running on port: 3000`);
 });
+
+
+```
+to actually print out a file path
+=> console.log(__dirname)
+
+in terminal it is
+=> pwd
+
+```
