72f39515822992c760ea6e0a5ea69be781219dbd
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-10-13 09:32:30 +0200

    updated the port code

--- calculator.js
+++ calculator.js
@@ -21,8 +21,8 @@ app.post("/", function(req, res){
 })
 
 
-app.listen(3000, function(){
-    console.log(`server running on port: 3000`);
+app.listen(2222, function(){
+    console.log(`server running on port: 2222`);
 });
 
 
